6078fe5de854cb7dc86167a38e6f7ea8a06cc205
mgreis <mario_a_pereira@hotmail.com>
2015-11-12 17:04:33 +0000

    [ADD] MapEntry.java

---
+++ Project2/Project2-ejb/src/java/is/project2/ejb/MapEntry.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package is.project2.ejb;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Mário
+ */
+final class MapEntry <Long,String> implements Map.Entry<Long,String>{
+    private final Long key;
+    private String value;
+    
+    public MapEntry(Long key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public Long getKey() {
+        return key;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String setValue(String value) {
+        String old = this.value;
+        this.value = value;
+        return old;
+    }
+}
